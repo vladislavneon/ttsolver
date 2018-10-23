7c754cdac94053334a8878a2ef90e1f81e54128d
Vladislav Korablinov <ne0n@MacBook-Air-Vladislav.local>
2018-10-23 13:33:47 +0300

    Added dummy solve.py

--- ml-server/solve.py
+++ ml-server/solve.py
@@ -1,2 +1,27 @@
-def solve(questions, text):
-    return []
+import json
+import random
+
+random.seed()
+
+def solve(questions_json, text):
+
+    answers_json = {"answers": []}
+
+    for q in questions_json["questions"]:
+        cur_answer = {
+                        "question": "",
+                        "verdict": "",
+                        "options": [],
+                        "answers": [],
+                        "answer_area": 0
+                    }
+        cur_answer["question"] = q["question"]
+        cur_answer["verdict"] = "ok"
+        cur_answer["answer_area"] = 0
+        for option in q["options"]:
+            cur_answer["options"].append(option)
+            cur_answer["answers"].append(random.randint(0, 1))
+        answers_json["answers"].append(cur_answer)
+
+    return answers_json
+    
