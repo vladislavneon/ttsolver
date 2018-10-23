40089e6b4d2d7142542e6372b8f9b6e5b9cdcb7c
Vladislav Korablinov <vladislavneon1@mail.ru>
2018-10-23 17:50:57 +0300

    minor fix in solve()

--- ml-server/solve.py
+++ ml-server/solve.py
@@ -15,7 +15,7 @@ def solve(questions, text):
         for option in q.options:
             options.append(option)
             answers.append(random.randint(0, 1))
-        answer.append(Answer(q.question, AnswerVerdict.ok, options, answers, random(0, max_line)))
+        answer.append(Answer(q.question, AnswerVerdict.ok, options, answers, random.randint(0, max_line)))
 
     return answer
 
