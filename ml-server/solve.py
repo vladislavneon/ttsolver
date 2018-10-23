454eacb0c9ca0a7c53a84b1c5492e648b413deb3
Vladislav Korablinov <vladislavneon1@mail.ru>
2018-10-24 00:20:34 +0300

    Improved answer area search

--- ml-server/solve.py
+++ ml-server/solve.py
@@ -1,7 +1,7 @@
 from model import Answer, AnswerVerdict, Question, QuestionType
 from tokenizer import tokenize_text, tokenize_string
 from normalizer import normalize_text, normalize_string
-from simple_search import find_best_single_choice, best_recalls
+from simple_search import find_best_single_choice, best_recalls, find_best_place
 import random
 
 def solve(questions, text):
@@ -24,7 +24,11 @@ def solve(questions, text):
                 choices[best_choice[2]] = 1
                 answers.append(Answer(question.question, AnswerVerdict.ok, question.options, choices, line_number))
             else:
-                answers.append(Answer(question.question, AnswerVerdict.found_area, question.options, choices, line_number))
+                best_place = find_best_place(text_with_lines, question_text, question_answers)[1]
+                if best_place != -1:
+                    answers.append(Answer(question.question, AnswerVerdict.found_area, question.options, choices, best_place))
+                else:
+                    answers.append(Answer(question.question, AnswerVerdict.fail, question.options, choices, best_place))
         else:
             choices = [random.randint(0, 1) for i in range(len(question.options))]
             answers.append(Answer(question.question, AnswerVerdict.fail, question.options, choices, 0))
