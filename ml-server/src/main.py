8fd28f468c5b5fa47124f41676c263a1e7d2761e
Vladislav Korablinov <vladislavneon1@mail.ru>
2018-10-24 23:20:09 +0300

    fixed division by zero

--- ml-server/src/main.py
+++ ml-server/src/main.py
@@ -3,13 +3,13 @@ from model import Question, QuestionType
 from aux import json_to_question, answers_to_json, export_tokenized_text, json_to_human
 from tokenizer import get_text_from_file
 
-questions = json_to_question('1_tests.json')
-text = get_text_from_file('text1.txt')
+questions = json_to_question('3_tests.json')
+text = get_text_from_file('text3.txt')
 
 a = solve(questions, text)
 
-answers_to_json(a, 'out1.json')
+answers_to_json(a, 'out3.json')
 
-json_to_human('1_tests.json')
+#json_to_human('1_tests.json')
 
 #export_tokenized_text('text2.txt')
--- ml-server/src/simple_search.py
+++ ml-server/src/simple_search.py
@@ -1,19 +1,21 @@
 import operator
-from tokenizer import get_tokenized_text_from_file, tokenize_string
-from normalizer import normalize_text, normalize_string
 
 
 multichoice_recall_threshold = 0.3
 singlechoice_recall_threshold_delta = 0.05
 singlechoice_recall_threshold = 0.2
 
+
 def count_recall(text_fragment, goal):
+    if not len(goal):
+        return 0
     n = len(goal)
     m = 0
     for word in goal:
         if word in text_fragment:
             m += 1
     return m / n
+
 
 def find_best_recall(text_with_lines, question, answer, q_weight=1, chunk_size=10):
     a_weight = 2 - q_weight
@@ -27,7 +29,6 @@ def find_best_recall(text_with_lines, question, answer, q_weight=1, chunk_size=1
         if recall > best_recall:
             best_recall = recall
             best_line_number = line_number
-            #best_line_number = i
     return (best_recall, best_line_number)
 
 
@@ -46,10 +47,12 @@ def best_recalls(text_with_lines, question, answers):
     best_recalls = [(br[0], br[1], i) for i, br in enumerate(best_recalls)]
     return best_recalls
 
+
 def find_best_multi_choice(text_with_lines, question, answers):
     best_recalls = [find_best_recall(text_with_lines, question, answer, q_weight=1.2, chunk_size=50) for answer in answers]
     best_recalls = [(br[0], br[1], i) for i, br in enumerate(best_recalls)]
     return list(filter(lambda x: x[0] > multichoice_recall_threshold, best_recalls))
+
 
 def find_best_place(text_with_lines, question, answers, chunk_size=30):
     question_summary = question
@@ -65,19 +68,3 @@ def find_best_place(text_with_lines, question, answers, chunk_size=30):
             best_recall = recall
             best_line_number = line_number
     return (best_recall, best_line_number)
-
-#
-# text_with_lines = normalize_text(get_tokenized_text_from_file('text2.txt'))
-# #print(text_with_lines)
-# question = normalize_string(tokenize_string('Основатель йоги?'))
-# print(question)
-# answer2 = normalize_string(tokenize_string('Патанджали'))
-# answer1 = normalize_string(tokenize_string('Ганди'))
-# answer3 = normalize_string(tokenize_string('Конфуций'))
-# answers = [answer1, answer2, answer3]
-# print(answers)
-# find_best_place(text_with_lines, question, answers)
-# # print(find_best_single_choice(text_with_lines, question, answers))
-# # print(find_best_multi_choice(text_with_lines, question, answers))
-# #print(len(text_with_lines))
-# #print(find_best_recall(text_with_lines[:2000], question, answer))
